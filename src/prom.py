13fa7b9903cdfd320b78aa007841da8268e09753
Andrée Hansson <peolanha@gmail.com>
2018-03-13 23:40:26 +0100

    Fix comments in prom.yml

--- src/prom.py
+++ src/prom.py
@@ -7,7 +7,7 @@ scrape_timestamp = None
 def build_metadata(results):
   """
   Builds up the MetaData object needed by engine to figure out which fields
-  the data contain, and what type they are (we default to strings here).
+  the data contain.
   """
 
   print('Building metadata...')
@@ -23,20 +23,23 @@ def build_metadata(results):
   for field in fields:
     metadata.fieldInfo.extend([connector_pb2.FieldInfo(name = field)])
 
-  #print('Metadata built {}'.format(metadata.fieldInfo))
-
   return metadata
 
 def set_value(result, fieldName, chunk):
   is_string = True
   value = None
+
   if fieldName == 'name':
+    # we normalize the metric name from __name__
     value = result['metric'].get('__name__', '')
-    #print(value)
   elif fieldName == 'timestamp':
+    # timestamp is the first value from each metric, and
+    # indicates when the value was scraped from a service
     is_string = False
     value = result['value'][0]
   elif fieldName == 'value':
+    # the actual value, we try to parse this as float but
+    # default to string
     value = result['value'][1]
     try:
       value = float(value)
@@ -44,23 +47,30 @@ def set_value(result, fieldName, chunk):
     except ValueError:
       pass
   elif fieldName == 'scrape_timestamp':
+    # we include a specific field for when this connector
+    # scraped the information from Prometheus
     value = scrape_timestamp
     is_string = False
   else:
+    # all other metric fields are dynamically read in, and
+    # we try to parse it as float and default to string
     value = result['metric'].get(fieldName, '')
     if type(value) is float:
       is_string = False
 
   if is_string:
     if len(value) == 0:
+      # empty values should be sent as null values
       chunk.stringCodes.append(-1)
       chunk.stringBucket.append('')
       chunk.numberCodes.append(-1)      
     else:
+      # string values
       chunk.stringCodes.append(len(chunk.stringBucket))
       chunk.stringBucket.append(str(value))
       chunk.numberCodes.append(-1)
   else:
+      # float values
     chunk.numberCodes.append(len(chunk.doubleBucket))
     chunk.doubleBucket.append(float(value))
     chunk.stringCodes.append(-1)
@@ -72,15 +82,11 @@ def build_chunks(results, metadata):
   the streaming gRPC protocol.
   """
 
-  print('Building data chunks...')
-  print(metadata.fieldInfo)
   for result in results:
     chunk = connector_pb2.DataChunk()
     for field in metadata.fieldInfo:
       set_value(result, field.name, chunk)
     yield chunk
-
-  print('Data chunks built')
 
 def fetch(prom_url, query_expr):
   """
@@ -88,13 +94,10 @@ def fetch(prom_url, query_expr):
   data structures.
   """
 
-  print('Querying Prometheus...')
   global scrape_timestamp
   scrape_timestamp = int(datetime.now(tz=timezone.utc).timestamp() * 1000)
 
   response = requests.get('{0}/api/v1/query'.format(prom_url), params={ 'query': query_expr })
   results = response.json()['data']['result']
-
-  print('Query complete')
 
   return results
